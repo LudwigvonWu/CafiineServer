b513d7eb65e6ab041b1654bab328f4ede1325439
Ray Koopa <raykoopa@users.noreply.github.com>
2017-01-07 13:30:03 +0100

    Increase version to 1.0.1

--- src/Syroot.CafiineServer/Properties/AssemblyInfo.cs
+++ src/Syroot.CafiineServer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Syroot.CafiineServer")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("1724f7f5-e495-4d15-a0fb-72ed4dca56b1")]
